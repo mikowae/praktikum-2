2bc173d54129b1579e051987ceef4d1b00d80711
firzy <firzy.wijatmiko.ft23@mail.umy.ac.id.com>
2024-03-14 17:40:19 +0700

    Add project files.

---
+++ praktikum 2.cpp
@@ -0,0 +1,27 @@
+
+#include <iostrime>{
+using namespace std;
+
+
+void insertionsort() {
+	int i, n, temp, j;
+	int arr[9];
+
+	for (i = 1; 1 <= n - 1;  i++) {//
+
+		temp = arr[i]; //step 2
+
+		j = i - 1; //step 3
+
+		while (j >= 0 && arr[j] > temp) // step 4
+		{
+
+			arr[j + i] = arr[j]; // step 4a
+			j--; // step 4b
+
+}
+		arr[j + 1] = temp; // step 5
+int maint()
+{
+
+}
